90433fb50b46b8186532fad313bc4d34cdd6b74f
Caique Moraes <ike.moraes93@gmail.com>
2020-10-12 14:46:37 -0300

    Updates

---
+++ precache-manifest.af6e85b15fa951f30b24626115823bd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760d9904feb92bdc83a626775badf362",
+    "url": "/search_countries/index.html"
+  },
+  {
+    "revision": "53c354baf18be1af14a5",
+    "url": "/search_countries/static/js/2.6b04b528.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/search_countries/static/js/2.6b04b528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce15f432d739d8e0f3a",
+    "url": "/search_countries/static/js/main.02890ab5.chunk.js"
+  },
+  {
+    "revision": "4ec8224d74e58f7e3690",
+    "url": "/search_countries/static/js/runtime-main.570cb58d.js"
+  },
+  {
+    "revision": "7d74678e8f4f3b126d8ac4982c862266",
+    "url": "/search_countries/static/media/404.7d74678e.svg"
+  },
+  {
+    "revision": "f6f2a198929c6496ca7cde01ca30cd43",
+    "url": "/search_countries/static/media/arrow_left.f6f2a198.svg"
+  },
+  {
+    "revision": "943ce6094f7651d828438e58f3aa888e",
+    "url": "/search_countries/static/media/moon.943ce609.svg"
+  },
+  {
+    "revision": "9873f8dbcb91af4c3b3728f8545a7cc7",
+    "url": "/search_countries/static/media/search.9873f8db.svg"
+  }
+]);
